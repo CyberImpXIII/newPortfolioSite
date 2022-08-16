6aa29a44f1592e15765d2d4f1aa5d09bff687c30
CyberImpXIII <jnelsenepstein@gmail.com>
2022-08-16 16:39:00 -0400

    four working circles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,9 +4,12 @@ import '../styles/App.css';
 function App() {
   return (
     <div className="App">
+      <div className='circleContainer'>
       <div className='circle'></div>
       <div className='circle' id='circle2'></div>
       <div className='circle' id='circle3'></div>
+      <div className='circle' id='circle4'></div>
+      </div>
     </div>
   );
 }
